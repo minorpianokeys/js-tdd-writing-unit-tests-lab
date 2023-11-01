249c625a573c1fbe1d299a69b01b93680a0766aa
Michael Friedman <minorpianokeys@gmail.com>
2023-11-01 11:40:44 -0400

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,11 @@
 // Your tests here
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome", () => {
+    it("returns true if word is a palindrome", () => {
+        const word = "racecar";
+
+        const f = isPalindrome(word)
+        expect(f).toBe(true);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
 // Your code here
+export function isPalindrome(word) {
+    // Convert the word to lowercase to ignore case sensitivity
+    word = word.toLowerCase();
+  
+    // Remove spaces from the word
+    word = word.replace(/\s/g, '');
+  
+    // Check if the word is equal to its reverse
+    return word === word.split('').reverse().join('');
+  }
